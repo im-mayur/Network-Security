a77c24f5ea2a6c123c471566e7060f77ac7631b7
im-mayur <mayurkulkarni5113@gmail.com>
2025-02-22 16:11:11 +0530

    model

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-from networksecurity.components.data_injestion import DataIngestion
-from networksecurity.components.data_validation import DataValidation
-from networksecurity.exception.exception import NetworkSecurityException
-from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataInjestionConfig,DataValidationConfig
-from networksecurity.constant import training_pipeline  
-from networksecurity.entity.config_entity import TrainingPipelineConfig
-import sys
+import dagshub
+dagshub.init(repo_owner="im-mayur", repo_name="Network-Security", mlflow=True)
 
-print("         ")
